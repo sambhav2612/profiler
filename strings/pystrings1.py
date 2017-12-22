87519d64e86da0f4b1a121dcbb00f75f7613e0df
Sambhav Jain <sambhavjain2612@gmail.com>
2017-12-22 23:08:18 +0530

    update

--- strings/pystrings1.py
+++ strings/pystrings1.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+"""Hello"""
 # strr = 'Sambhav Jain'
 # print strr
 # strr[7] = 'X'   #immuntable
@@ -25,14 +27,30 @@ b = a.rfind("a")
 print b          # return last index to substring
 
 # if-else in working
-if (a.startswith("S") & a.endswith("n")):
+if a.startswith("S") & a.endswith("n"):
     print "a starts with S and ends with n"
 else : print "a does not starts with S and ends with n"
 
+
+# for-loop in working
 xx = 1
+alphas = 0
+nums = 0
+spaces = 0
+
+a += single + double + triple + "0123456789"
+
+print "Length Size of a is = %d" % len(a)
+
 for x in range(0, len(a)):
     if a[x].islower():
         xx += 1
     else: xx -= 1
-    if (xx % 2 == 0):
-        print "Printing count only when it's even", xx
+    if a[x].isalpha():
+        alphas += 1
+    if a[x].isdigit():
+        nums += 1
+    if a[x].isspace():
+        spaces += 1
+print "Number of lower case charcaters found: %d" % xx
+print "Number of Digits, Spaces and Alphabests found till index %d: %d %d %d" % (x, nums, spaces, alphas) 
